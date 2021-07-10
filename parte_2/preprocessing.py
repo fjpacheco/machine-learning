32c841a71d0e9e222fca2d738d1914bc5e4f8f22
axelkelman <akelman@fi.uba.ar>
2021-07-10 17:14:04 -0300

    agrego función necesaria

--- parte_2/preprocessing.py
+++ parte_2/preprocessing.py
@@ -452,14 +452,41 @@ def graficar_matriz_confusion(y_true, y_pred):
     plt.show()
 
 
-def plot_roc_curves(clf, XX_test, yy_test, XX_train, yy_train):
+def plot_roc_curves_red(clf, XX_test, yy_test, XX_train, yy_train):
     plt.figure(dpi=110)
 
     # For Test
-    fpr_test, tpr_test, _ = roc_curve(yy_test, clf.predict_proba(XX_test)[:, 1])
+    fpr_test, tpr_test, _ = roc_curve(yy_test, clf.predict(XX_test))
     roc_auc_test = auc(fpr_test, tpr_test)
     # For Train
-    fpr_train, tpr_train, _  = roc_curve(yy_train, clf.predict_proba(XX_train)[:, 1])
+    fpr_train, tpr_train, _  = roc_curve(yy_train, clf.predict(XX_train))
+    roc_auc_train = auc(fpr_train, tpr_train)
+
+    # Ploting
+    plt.plot(
+        fpr_test, tpr_test, color='red', lw=1, label=f'ROC curve for Test (area = {roc_auc_test:.2f})'
+    )
+    plt.plot(
+        fpr_train, tpr_train, color='green', lw=1, label=f'ROC curve for Train (area = {roc_auc_train:.2f})'
+    )
+
+    plt.plot([0, 1], [0, 1], color='navy', lw=1, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate', weight= "bold")
+    plt.ylabel('True Positive Rate', weight= "bold")
+    plt.title('Curva ROC', weight= "bold")
+    plt.legend(loc="lower right")
+    plt.show()
+    
+    def plot_roc_curves(clf, XX_test, yy_test, XX_train, yy_train):
+    plt.figure(dpi=110)
+
+    # For Test
+    fpr_test, tpr_test, _ = roc_curve(yy_test, clf.predict(XX_test))
+    roc_auc_test = auc(fpr_test, tpr_test)
+    # For Train
+    fpr_train, tpr_train, _  = roc_curve(yy_train, clf.predict(XX_train))
     roc_auc_train = auc(fpr_train, tpr_train)
 
     # Ploting
